a0ac4ab8dc8c2a62ce3da7bfd0b1f5f0ac4790ce
Alex-Grigorovich <holdencolfield37@gmail.com>
2023-11-15 22:00:10 +0300

    Add GUI and logic hearts.
    Add GUI and logic hearts.

--- Levels/GameRules.gd
+++ Levels/GameRules.gd
@@ -3,13 +3,14 @@ extends Node
 var lives = 3
 
 func _ready():
-	pass
+	get_tree().call_group("GUI", "add_lives", lives)
 
 func hurt():
 	$Player.hurt()
 	lives -= 1
 	if lives < 1:
 		end_game()
+	get_tree().call_group("GUI", "remove_lives", lives)
 
 func end_game():
 	get_tree().change_scene("res://GUI/GameOver.tscn")
---
+++ Scripts/GUI.gd
@@ -0,0 +1,14 @@
+extends CanvasLayer
+
+
+func _ready():
+	pass
+
+func add_lives(lives_count):
+	while lives_count > $Control/HeartsContainer.get_child_count():
+		$Control/HeartsContainer.add_child(load("res://GUI/heart1.tscn").instance())
+	
+func remove_lives(lives_count):
+	if lives_count < $Control/HeartsContainer.get_child_count():
+		var heart = $Control/HeartsContainer.get_child(0)
+		heart.queue_free()
